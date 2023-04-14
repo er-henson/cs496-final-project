3e5b0ba871704652504cb56cc2fdce23ef8b6711
Higgs32584 <mjhiggins2@loyola.edu>
2023-04-14 17:13:12 -0400

    [#36] is DONE

--- my-app/src/Components/Footer.js
+++ my-app/src/Components/Footer.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import '../App.css'
-function Footer()
-{
-    return (
+import '../App.css';
+
+function Footer() {
+  return (
     <div className="jumbotron mt-2 p-4 text-center text-white">
-    <p >
-  Contact us at: 555-555-5555<br/>
-  prssaloy@loyola.edu
-    </p>
+      <p>
+        Contact us at: 555-555-5555<br />
+        prssaloy@loyola.edu
+        <br />
+        <a href="https://www.prsa.org/prsa" target="_blank" rel="noopener noreferrer">
+          Visit our main site
+        </a>
+      </p>
     </div>
-    );
-};
+  );
+}
 
 export default Footer;
-
 
